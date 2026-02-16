25f4567ec5caf3a1eb47a1deb97eff759417bc7f
shivamkachhadiya <shivamkachhadiya68@gmail.com>
2026-02-16 17:46:41 +0530

    Initial commit

---
+++ blog/js/main.js
@@ -0,0 +1,11 @@
+function openImage(img) {
+    const modal = document.getElementById("imgModal");
+    const modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+}
+
+function closeImage() {
+    document.getElementById("imgModal").style.display = "none";
+}
